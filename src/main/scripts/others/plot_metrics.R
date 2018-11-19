7fe6d924ea7d40d4b436730acc78cc2d8bee2fce
akm0001 <anandmaurya@hotmail.com>
2018-11-19 16:35:50 +0100

    modified the grid in the plot

--- src/main/scripts/others/plot_metrics.R
+++ src/main/scripts/others/plot_metrics.R
@@ -7,6 +7,7 @@ par(mfrow=c(1,2))
 blastFull<- read.table("/home/anand/Documents/resources/test.plot.inp.txt",header = F,sep = "\t")
 blastFiltered<- read.table("/home/anand/Documents/resources/test.plot.inp.2.txt",header = F,sep = "\t")
 
+
 # Compute the density data
 densPlotFull <- density(blastFull$V5)
 # plot density
@@ -16,4 +17,5 @@ densPlotFiltered <- density(blastFiltered$V5)
 # plot density
 plot(densPlotFiltered, frame = FALSE, col = "steelblue", main = "Blastn (filtered)") 
 
-
+png('/home/anand/Documents/resources/coveragePlot.png')
+dev.off()
